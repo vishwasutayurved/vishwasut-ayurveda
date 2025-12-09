dc6892a025fc5d4c56e7c016057c75ec07e0cd6a
My Life <vloglife643@gmail.com>
2025-12-09 08:20:41 +0000

    firebase setup

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,10 +2,10 @@
 // Follow this guide to obtain your Firebase config object:
 // https://firebase.google.com/docs/web/setup#get-config-object
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAcaaUuawQUtA9feKWNJSMkGJZp0xbL_H0",
+  authDomain: "vishwasut-ayurved.firebaseapp.com",
+  projectId: "vishwasut-ayurved",
+  storageBucket: "vishwasut-ayurved.firebasestorage.app",
+  messagingSenderId: "405199693218",
+  appId: "1:405199693218:web:5b3c7c737a449c04917efb"
 };
