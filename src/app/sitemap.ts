80d1341465cedd60d0f32979e2f6eefd57a6681d
vishwasutayurved <vishwasutayurved@gmail.com>
2025-12-11 13:21:57 +0000

    added sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,37 @@
+import type { MetadataRoute } from 'next'
+
+export const dynamic = "force-dynamic";
+const appUrl: string = "https://vishwasutayurveda.vercel.app"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: appUrl,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
+        },
+        {
+            url: appUrl + '/about',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
+        },
+        {
+            url: appUrl + '/contact',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
+        }, {
+            url: appUrl + '/products',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
+        }, {
+            url: appUrl + '/therapies',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
+        }
+    ];
+}
