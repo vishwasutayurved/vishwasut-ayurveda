769782db66c7c794cfc522b6e45042ba9e9b1488
vishwasutayurved <vishwasutayurved@gmail.com>
2025-12-11 13:27:25 +0000

    added product in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -27,11 +27,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.3,
-        }, {
+        },
+        {
             url: appUrl + '/therapies',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.3,
+        },
+        {
+            url: appUrl + '/products/triphala-guggulu',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
         }
     ];
 }
