160d544d8248fe3f61ed3f0ea8aee023f081e6bb
vishwasutayurved <vishwasutayurved@gmail.com>
2026-01-17 09:42:23 +0000

    Twak.to implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,23 @@ export default function RootLayout({
           </div>
           <Toaster />
         </ThemeProvider>
+        <Script
+          id="tawk-to-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/696b52acc4b651197faa9643/1jf5jlska';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+              `,
+          }}
+        />
       </body>
     </html>
   );
