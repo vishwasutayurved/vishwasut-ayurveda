bf8265ff4eb3230737687cd432a4cab53f56c10b
vishwasutayurved <vishwasutayurved@gmail.com>
2026-01-19 09:11:10 +0000

    auto Sitemap generation without sitemap index.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://vishwasutayurveda.web.app',
+    generateRobotsTxt: true,
     trailingSlash: true,
+    generateIndexSitemap: false,
   };
   
