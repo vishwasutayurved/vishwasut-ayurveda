c4e94d09400a9b13af4f09c6ed337a5975b475fa
vishwasutayurved <vishwasutayurved@gmail.com>
2026-02-03 14:12:42 +0000

    updated image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'sgp.cloud.appwrite.io',
+      },
     ],
   },
 };
