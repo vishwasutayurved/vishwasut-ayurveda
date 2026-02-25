94773c2f23065a8c749096eba51a9fd6f225d98a
vishwasutayurved <vishwasutayurved@gmail.com>
2026-02-25 12:11:56 +0000

    isNext

--- src/lib/pushya-nakshatra-dates.ts
+++ src/lib/pushya-nakshatra-dates.ts
@@ -4,53 +4,62 @@ export const upcomingPushyaNakshatraDates = [
         day: "Saturday-Sunday", 
         starts: "09:35 AM", 
         ends: "08:35 AM",
+        isNext : false
     },
     { 
         date: "27 & 28-Mar-2026", 
         day: "Friday-Saturday", 
         starts: "03:24 PM", 
-        ends: "02:50 PM" 
+        ends: "02:50 PM" ,
+        isNext : false
     },
     { 
         date: "23 & 24-Apr-2026", 
         day: "Thursday-Friday", 
         starts: "08:58 PM", 
-        ends: "08:15 PM" 
+        ends: "08:15 PM" ,
+        isNext : false
     },
     { 
         date: "21-May-2026", 
         day: "Thursday", 
         starts: "04:13 AM", 
-        ends: "11:59 PM" 
+        ends: "11:59 PM" ,
+        isNext : false
     },
     { 
         date: "17 & 18-Jun-2026", 
         day: "Wednesday-Thursday", 
         starts: "01:38 PM", 
-        ends: "11:33 AM" 
+        ends: "11:33 AM" ,
+        isNext : false
     },
     { 
         date: "15-Jul-2026", 
         day: "Wednesday", 
         starts: "12:10 AM", 
-        ends: "09:47 PM" 
+        ends: "09:47 PM" ,
+        isNext : false
     },
     { 
         date: "11 & 12-Aug-2026", 
         day: "Tuesday-Wednesday", 
         starts: "10:10 AM", 
-        ends: "08:00 AM" 
+        ends: "08:00 AM" ,
+        isNext : false
     },
     { 
         date: "07 & 08-Sep-2026", 
         day: "Monday-Tuesday", 
         starts: "06:14 PM", 
-        ends: "04:40 PM" 
+        ends: "04:40 PM" ,
+        isNext : false
     },
     { 
         date: "05-Oct-2026", 
         day: "Monday", 
         starts: "12:14 AM", 
-        ends: "11:10 PM" 
+        ends: "11:10 PM" ,
+        isNext : false
     },
 ];
