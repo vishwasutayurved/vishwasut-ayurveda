0994880738771f8ddb5441aa4ec57535dd4b4d81
vishwasutayurved <vishwasutayurved@gmail.com>
2026-04-22 14:37:08 +0000

    Updated the website opening statement

--- src/components/home/hero-banner.tsx
+++ src/components/home/hero-banner.tsx
@@ -42,7 +42,7 @@ export function HeroBanner() {
                 Because you’re treating symptoms — not the cause.
             </p>
             <p className="mt-6 text-sm md:text-lg leading-relaxed text-foreground/80">
-              From <strong className='text-primary'>Gut issues</strong> and stress to <strong className='text-primary'>PCOD</strong>, <strong className='text-primary'>diabetes</strong>, <strong className='text-primary'>skin</strong>, <strong className='text-primary'>spine</strong>, and <strong className='text-primary'>infertility</strong> — our Ayurvedic approach targets the root to restore real health.
+              From <strong className='text-primary'>Gut Issues</strong> and <strong>Stress</strong> to <strong className='text-primary'>PCOD</strong>, <strong className='text-primary'>Diabetes</strong>, <strong className='text-primary'>Skin</strong>, <strong className='text-primary'>Spine</strong>, and <strong className='text-primary'>Infertility</strong> — our Ayurvedic approach targets the root to restore real health.
             </p>
         </div>
 
